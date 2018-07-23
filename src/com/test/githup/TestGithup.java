02a7a093b53904d2e4b76e498b5c4cbedf609ecd
xianganxiao <1712183166@qq.com>
2018-07-23 15:50:22 +0800

    测试 githup类 提交

--- src/com/test/githup/TestGithup.java
+++ src/com/test/githup/TestGithup.java
@@ -1,7 +1,7 @@
 package com.test.githup;
 
-public class TestDemo {
+public class TestGithup {
 	public static void main(String[] args) {
-		System.out.println("hello world 11");
+		System.out.println("test githup");
 	}
 }
