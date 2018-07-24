5f23842ad7b7bee08a5932255aedfe72d5db5e3f
Administrator <Administrator@EDZ-20180505XLY>
2018-07-24 10:05:33 +0800

    提交代码

--- src/com/test/githup/TestDemo.java
+++ src/com/test/githup/TestDemo.java
@@ -2,6 +2,6 @@ package com.test.githup;
 
 public class TestDemo {
 	public static void main(String[] args) {
-		System.out.println("hello world 477777");
+		System.out.println("hello world 499999");
 	}
 }
